87591332028f96e349560eec176e1fdc32cb92f1
Andrew Zito <azito122@gmail.com>
2017-01-31 15:54:27 -0500

    v1.1.3-frontend: main overlay, navbar

--- index.php
+++ index.php
@@ -4,6 +4,18 @@
 	<?php include 'head.php' ?>
 	
 	<body>
-	<?php include 'app/workspace-div.php' ?>
+		<div id="main">
+			<div class="container container_logo">
+				<h1>SYNTREE</h1>
+			</div>
+			<nav class="navbar navbar_main">
+				<a class="navbar_link navbar_link_left" href="#"><span>Home</span></a>
+				<a class="navbar_link navbar_link_left" href="#"><span>Make a syntax tree!</span></a>
+				<a class="navbar_link navbar_link_right" href="#"><span>Make an account</span></a>
+				<a class="navbar_link navbar_link_right" href="#"><span>Login</span></a>
+			</nav>
+	
+			<?php //include 'app/workspace-div.php' ?>
+		</div>
 	</body>
 </html>
