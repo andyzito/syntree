d39666e66fdcc6cf1da514057c57aee6bcb1cd64
Andrew Zito <azito122@gmail.com>
2017-02-02 02:12:00 -0500

    v1.2.4: create new user is functional

--- classes/db.php
+++ classes/db.php
@@ -1,23 +1,26 @@
 <?php
 include $_SERVER['DOCUMENT_ROOT'] . '/syntree/db/db-config.php';
 
-class DB {
-	function __construct($dbhost,$dbuser,$dbpass,$dbname) {
-		$this->db = new mysqli($dbhost,$dbuser,$dbpass,$dbname);
-	}
+if (!class_exists('DB')) {
+	class DB {
+		function __construct($dbhost,$dbuser,$dbpass,$dbname) {
+			$this->db = new mysqli($dbhost,$dbuser,$dbpass,$dbname);
+		}
 
-	public function raw_query($sql) {
-		return $this->db->query($sql);
-	}
+		public function raw_query($sql) {
+			return $this->db->query($sql);
+		}
 
-	public function get_user($name) {
-		$sql = "SELECT * FROM user WHERE username='$name'";
-		return $this->db->query($sql)->fetch_assoc();
-	}
+		public function get_user($name) {
+			$sql = "SELECT * FROM user WHERE username='$name'";
+			return $this->db->query($sql)->fetch_assoc();
+		}
 
-	public function create_user($id,$uname,$pass,$email,$fname,$lname) {
-		$sql = "INSERT INTO user (id,username,password,email,firstname,lastname) VALUES('$id','$uname','$pass','$email','$fname','$lname')";
-		return $this->db->query($sql);
+		public function create_user($id,$uname,$pass,$email,$fname,$lname) {
+			$sql = "INSERT INTO user (id,username,password,email,firstname,lastname) VALUES($id,'$uname','$pass','$email','$fname','$lname')";
+			echo $sql;	
+			return $this->db->query($sql);
+		}
 	}
 }
 
--- post/new-user.php
+++ post/new-user.php
@@ -1,8 +1,11 @@
 <?php
 include $_SERVER['DOCUMENT_ROOT'] . '/syntree/classes/db.php';
+include $_SERVER['DOCUMENT_ROOT'] . '/syntree/gen-id.php';
+
 $uname = $_POST['username'];
 $pass = $_POST['password'];
+$id = gen_id(1);
 
-$result = $DB->create_user(42,$uname,$pass,'','','');
+$result = $DB->create_user($id,$uname,$pass,'','','');
 
 echo $result;
