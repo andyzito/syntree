10e9357b1e8fe203a33db50cb6071ea6a8fe3992
Andrew Zito <azito122@gmail.com>
2017-02-01 20:44:46 -0500

    v1.1.4-frontend: added fullpage app page

--- index.php
+++ index.php
@@ -6,16 +6,17 @@
 	<body>
 		<div id="main">
 			<div class="container container_logo">
-				<h1>SYNTREE</h1>
+				<img src="syntree-logo.png">
 			</div>
 			<nav class="navbar navbar_main">
-				<a class="navbar_link navbar_link_left" href="#"><span>Home</span></a>
-				<a class="navbar_link navbar_link_left" href="#"><span>Make a syntax tree!</span></a>
+				<a class="navbar_link navbar_link_left" href="index.php"><span>Home</span></a>
+				<a class="navbar_link navbar_link_left" href="make.php"><span>Make a syntax tree!</span></a>
 				<a class="navbar_link navbar_link_right" href="#"><span>Make an account</span></a>
 				<a class="navbar_link navbar_link_right" href="#"><span>Login</span></a>
 			</nav>
-	
-			<?php //include 'app/workspace-div.php' ?>
+
+			<h1>Try it! Right here:</h1>
+			<?php include 'app/workspace-div.php' ?>
 		</div>
 	</body>
 </html>
---
+++ make.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<link rel="stylesheet" href="css/css.css">
+	<link rel="stylesheet" href="app/css/css.css">
+	<?php include 'script-import.php' ?>
+</head>
+<body class="page_make">
+	<a href="index.php">
+		<div class="button button_back">
+		<< Back
+		</div>
+	</a>
+	<?php include 'app/workspace-div.php' ?>
+</body>
+</html>
