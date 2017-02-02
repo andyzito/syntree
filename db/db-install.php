50f86a2f7579d38fe919fc33a21c20d907345033
Andrew Zito <azito122@gmail.com>
2017-02-02 00:55:02 -0500

    v1.2.1 uid generation script

--- db/db-install.php
+++ db/db-install.php
@@ -4,3 +4,6 @@ include 'db-config.php';
 $db = new mysqli($dbhost,$dbuser,$dbpass,$dbname);
 
 $db->query('CREATE TABLE user (id INT(10), username VARCHAR(30), password VARCHAR(30), firstname VARCHAR(30), lastname VARCHAR(30), email VARCHAR(30))');
+
+$db->query('CREATE TABLE data (name VARCHAR(30), value TEXT)');
+$db->query("INSERT INTO data (name,value) VALUES ('ids','')");
---
+++ gen-id.php
@@ -0,0 +1,39 @@
+<?php
+function gen_id($n) {
+	include '/var/www/html/div3/classes/db.php';
+	
+	if ($n === 0) {
+		return;
+	} else if ($n === 1) {
+		$ids = $DB->raw_query("SELECT value FROM data WHERE name='ids'")->fetch_assoc()['value'];
+		$ids = explode(',', $ids);
+
+		while (true) {
+			$x = mt_rand(0,10000);
+			if (!in_array($x,$ids)) {
+				array_push($ids,strval($x));
+				$ids = implode($ids,',');
+				$DB->raw_query("UPDATE data SET value='$ids' WHERE name='ids'");
+				return $x;
+			}
+		}
+	} else {
+		$result = [];
+		$ids = $DB->raw_query("SELECT value FROM data WHERE name='ids'")->fetch_assoc()['value'];
+		$ids = explode(',', $ids);
+		$i = 0;
+
+		while ($i < $n) {
+			$x = mt_rand(0,10000);
+			if (!in_array($x,$ids)) {
+				array_push($ids,strval($x));
+				$temp_ids = implode($ids,',');
+				$DB->raw_query("UPDATE data SET value='$temp_ids' WHERE name='ids'");
+				array_push($result,$x);
+				$i = $i + 1;
+			}
+		}
+
+		return $result;
+	}
+}
